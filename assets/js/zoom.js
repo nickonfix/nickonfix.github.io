992fe82f61e62cc03d5740aa0428714ede6c62a6
nickonfix <143015079+nickonfix@users.noreply.github.com>
2026-09-04 14:56:14 +0000

    Deploying to gh-pages from @ nickonfix/nickonfix.github.io@4532e44e93bc64e20e90450a0c989bed7817d09b 🚀

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})});
+$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"}),medium_zoom.on("open",function(){document.body.classList.add("zoom-open")}),medium_zoom.on("closed",function(){document.body.classList.remove("zoom-open")})});
